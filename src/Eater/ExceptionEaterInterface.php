eb849014e60f8712b4001765d3897f8dd1cc1248
Andreas Hennings <andreas@dqxtech.net>
2016-05-30 03:46:57 +0200

    Add second parameter for message.

--- src/Eater/ExceptionEaterInterface.php
+++ src/Eater/ExceptionEaterInterface.php
@@ -2,14 +2,25 @@
 
 namespace Donquixote\Except\Eater;
 
+/**
+ * Interface for pluggable exception handlers.
+ *
+ * The term 'eater' may be misleading. But from the consuming component's
+ * perspective, this is exactly what it does. The eater takes the exception, and
+ * the consuming component can forget about it.
+ */
 interface ExceptionEaterInterface {
 
   /**
-   * Does something with an exception, so that other components don't have to
-   * deal with it anymore.
+   * Handles, or 'eats', an exception.
+   *
+   * A good implementation will not simply silence the exception, but do
+   * something useful, such as, writing to a log.
    *
    * @param \Exception $e
+   * @param string|null $message
+   *   (optional) Message explaining the consequences of the exception.
    */
-  public function eatException(\Exception $e);
+  public function eatException(\Exception $e, $message = NULL);
 
 }
